050f594e667f54259736c01cacca2f92a34387bb
ARWaller <100862048+ARWaller@users.noreply.github.com>
2023-10-21 17:34:52 -0400

    Update Equipment.java

--- Gym/src/gym/Equipment.java
+++ Gym/src/gym/Equipment.java
@@ -1,135 +1,78 @@
-
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
 package com.mycompany.databaseexample;
 
 /**
  *
  * @author antho
  */
-public class Members {
-    private int member_ID, total_visits;
-    private String first_name, last_name, join_date, last_visit, employee_sponsor, membership;
+public class Equipment {
+    private int equipment_ID, cost, daily_usage,employee_owner;
     
-    Members(int member_ID, String first_name, String last_name, String join_date, String last_visit, int total_visits, String employee_sponsor, String membership){
-        this. member_ID = member_ID;
-        this.first_name = first_name;
-        this.last_name = last_name;
-        this.join_date = join_date;
-        this.last_visit = last_visit;
-        this.total_visits = total_visits;
-        this.employee_sponsor = employee_sponsor;
-        this.membership = membership;
+    Equipment(int equipment_ID, int cost, int daily_usage, int employee_owner){
+        this.equipment_ID = equipment_ID;
+        this.cost = cost;
+        this.daily_usage = daily_usage;
+        this.employee_owner = employee_owner;
+    }
+        
+
+    /**
+     * @return the equipment_ID
+     */
+    public int getEquipment_ID() {
+        return equipment_ID;
     }
 
     /**
-     * @return the member_ID
+     * @param equipment_ID the equipment_ID to set
      */
-    public int getMember_ID() {
-        return member_ID;
+    public void setEquioment_ID(int equipment_ID) {
+        this.equipment_ID = equipment_ID;
     }
 
     /**
-     * @param member_ID the member_ID to set
+     * @return the cost
      */
-    public void setMember_ID(int member_ID) {
-        this.member_ID = member_ID;
+    public int getCost() {
+        return cost;
     }
 
     /**
-     * @return the total_visits
+     * @param cost the cost to set
      */
-    public int getTotal_visits() {
-        return total_visits;
+    public void setCost(int cost) {
+        this.cost = cost;
     }
 
     /**
-     * @param total_visits the total_visits to set
+     * @return the daily_usage
      */
-    public void setTotal_visits(int total_visits) {
-        this.total_visits = total_visits;
+    public int getDaily_usage() {
+        return daily_usage;
     }
 
     /**
-     * @return the first_name
+     * @param daily_usage the daily_usage to set
      */
-    public String getFirst_name() {
-        return first_name;
+    public void setDaily_usage(int daily_usage) {
+        this.daily_usage = daily_usage;
     }
 
     /**
-     * @param first_name the first_name to set
+     * @return the employee_owner
      */
-    public void setFirst_name(String first_name) {
-        this.first_name = first_name;
+    public int getEmployee_owner() {
+        return employee_owner;
     }
 
     /**
-     * @return the last_name
+     * @param employee_owner the employee_owner to set
      */
-    public String getLast_name() {
-        return last_name;
-    }
-
-    /**
-     * @param last_name the last_name to set
-     */
-    public void setLast_name(String last_name) {
-        this.last_name = last_name;
-    }
-
-    /**
-     * @return the join_date
-     */
-    public String getJoin_date() {
-        return join_date;
-    }
-
-    /**
-     * @param join_date the join_date to set
-     */
-    public void setJoin_date(String join_date) {
-        this.join_date = join_date;
-    }
-
-    /**
-     * @return the last_visit
-     */
-    public String getLast_visit() {
-        return last_visit;
-    }
-
-    /**
-     * @param last_visit the last_visit to set
-     */
-    public void setLast_visit(String last_visit) {
-        this.last_visit = last_visit;
-    }
-
-    /**
-     * @return the employee_sponsor
-     */
-    public String getEmployee_sponsor() {
-        return employee_sponsor;
-    }
-
-    /**
-     * @param employee_sponsor the employee_sponsor to set
-     */
-    public void setEmployee_sponsor(String employee_sponsor) {
-        this.employee_sponsor = employee_sponsor;
-    }
-
-    /**
-     * @return the membership
-     */
-    public String getMembership() {
-        return membership;
-    }
-
-    /**
-     * @param membership the membership to set
-     */
-    public void setMembership(String membership) {
-        this.membership = membership;
+    public void setEmployee_owner(int employee_owner) {
+        this.employee_owner = employee_owner;
     }
     
     
